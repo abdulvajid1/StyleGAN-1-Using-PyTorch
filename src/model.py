434e509e234b30906ac514f37227a7f2a61ea540
abdulvajid1 <vajidca444@gmail.com>
2025-11-29 08:31:21 +0530

    pixel norm & conv layer

--- src/model.py
+++ src/model.py
@@ -6,7 +6,7 @@ class FCLayer(nn.Module):
         super().__init__()
         self.fc = nn.Sequential(
             nn.Linear(dim, dim, bias=True),
-            nn.ReLU()
+            nn.LeakyReLU(0.2)
         )
     def forward(self, x):
         return self.fc(x)
@@ -36,6 +36,48 @@ class AdaIN(nn.Module):
         y_bias = style_params[: , self.out_channels: ].unsqueeze(-1).unsqueeze(-1) # (B, out_channels, 1, 1)
         print(y_scale.shape, y_bias.shape)
         return y_scale * ((x - x.mean(dim=[-2, -1], keepdim=True)) / x.std(dim=[-2, -1], keepdim=True) + self.eps) + y_bias
+
+class NoiseInjection(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.scale_b = nn.Parameter(torch.ones(channels))
+        
+    def forward(self, x: torch.Tensor):
+        batch_size, C, H, W= x.shape
+        noise = torch.randn((batch_size, 1, H, W), device=x.device)
+        scaled_noise = noise * self.scale_b.view(1, C, 1, 1)
+        return x + scaled_noise
+
+class PixelNorm(nn.Module):
+    def __init__(self, eps: float = 1e-8):
+        super().__init__()
+        # channels is unused but kept for API compatibility
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor):
+        # (b, c, h, w)
+        return x / (torch.sqrt(torch.mean(x*x, dim=1, keepdim=True)) + self.eps)
+
+      
+class ConvLayer(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size, padding, stride):
+        super().__init__()
+        self.conv_layer = nn.Sequential(
+            nn.Conv2d(in_channels=in_channel, 
+                      out_channels=out_channel, 
+                      kernel_size=kernel_size, 
+                      padding=padding, 
+                      stride=stride),
+            nn.LeakyReLU(0.02),
+            PixelNorm()
+            )
+    
+    def forward(self, x: torch.Tensor):
+        return self.conv_layer(x)
+
+        
+        
+
         
         
 
